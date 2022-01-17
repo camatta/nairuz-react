13c51b5d67ea3cb12e3fc0e6b412b5ce451aec79
CIANDT\wjunior <wjunior@ciandt.com>
2022-01-17 16:24:56 -0300

    Alteraçoes para funcionar gambiarra na home.

--- components/Headergeral.js
+++ components/Headergeral.js
@@ -1,20 +1,24 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+const urlHome = () => {
+  document.location.href = '/';
+}
+
 const Headergeral = () => (
   <header className="container-fluid" id="header-geral">
     <div className="container">
       <div className="row d-flex align-items-center">
         <div className="col-2">
           <div className="logo">
-            <Link href="/"><a><Image src="/logo-header.svg" alt="Logo Nairuz" width={150} height={28}/></a></Link>
+            <a onClick={urlHome}><Image src="/logo-header.svg" alt="Logo Nairuz" width={150} height={28}/></a>
           </div>
         </div>
         <div className="col-8">
           <div className="menu">
             <nav>
               <ul>
-                <li><Link href="/"><a>Home</a></Link></li>
+                <li><a onClick={urlHome}>Home</a></li>
                 <li><Link href="/quem-somos"><a>Quem Somos</a></Link></li>
                 <li><Link href="/servicos"><a>Serviços</a></Link></li>
                 <li><Link href="/cases"><a>Cases</a></Link></li>
