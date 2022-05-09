caaa55622d85b439ac6efb3cedf3e3c114bec2c3
Gabriel Camatta <contatogcamatta@gmail.com>
2022-05-09 14:23:41 -0300

    start hotjar LPs

--- pages/performance-digital-nz.js
+++ pages/performance-digital-nz.js
@@ -631,6 +631,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   } else {
     $('body').addClass('loaded');
   }
+
+  (function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:445751,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
   
   `}
 </Script>
