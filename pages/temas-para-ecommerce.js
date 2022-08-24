d226d1e554efb8499d9d7bc377062596ce835f49
luisgrange <luisfernando237grange@outlook.com>
2022-08-24 17:34:07 -0300

    Add vtex logo

--- pages/temas-para-ecommerce.js
+++ pages/temas-para-ecommerce.js
@@ -337,7 +337,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 <li><Image src="/plataforma-nexaasSigna.png" alt="Signativa Logo" width={128} height={29} /></li>
                 <li><Image src="/plataforma-linx.png" alt="Linx Logo" width={117} height={56} /></li>
                 <li><Image src="/plataforma-mage.png" alt="Mageshop Logo" width={117} height={56} /></li>
-                <li><Image src="/Vtex_Logo.svg" alt="Vtex Logo" width={128} height={30} /></li>
+                <li><Image src="/VTEX_Logo.svg" alt="Vtex Logo" width={128} height={30} /></li>
                 <li><Image src="/plataforma-bizcommerce.svg" alt="Bizcommerce Logo" width={128} height={30} /></li>
               </ul>
             </div>
