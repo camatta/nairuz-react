56db52bcf54863deada70714adac1cecea693624
camatta <contatogcamatta@gmail.com>
2023-01-10 10:20:58 -0300

    atualizacao insta e correcao tablet

--- components/Footergeral.js
+++ components/Footergeral.js
@@ -33,15 +33,15 @@ const Footergeral = () => (
   <>
   <footer className="container-fluid" id="footer">
   <div className="instagram-feed">
-    <ul className="insta d-flex justify-content-center">
-      <li><a href="https://www.instagram.com/p/CeQrqvAMG7K/" target="_blank" rel="noopener noreferrer"><Image src="/insta1.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
-      <li><a href="https://www.instagram.com/p/CeLh7fyMkxB/" target="_blank" rel="noopener noreferrer"><Image src="/insta2.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
-      <li><a href="https://www.instagram.com/p/CeDzqNfs7y0/" target="_blank" rel="noopener noreferrer"><Image src="/insta3.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
-      <li><a href="https://www.instagram.com/p/CeBOuwtOLP_/" target="_blank" rel="noopener noreferrer"><Image src="/insta4.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
-      <li><a href="https://www.instagram.com/p/Cd-qGN9smqO/" target="_blank" rel="noopener noreferrer"><Image src="/insta5.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
-      <li><a href="https://www.instagram.com/p/Cd8FTSAsb2d/" target="_blank" rel="noopener noreferrer"><Image src="/insta6.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
-      <li><a href="https://www.instagram.com/p/Cd5gmQeg97L/" target="_blank" rel="noopener noreferrer"><Image src="/insta7.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
-      <li><a href="https://www.instagram.com/p/CdxyCWDMhX4/" target="_blank" rel="noopener noreferrer"><Image src="/insta8.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
+    <ul className="insta d-flex justify-content-center" id="content-instagram">
+      <li><a href="https://www.instagram.com/p/CnO4xuYOjUu/" target="_blank" rel="noopener noreferrer"><Image src="/insta1.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
+      <li><a href="https://www.instagram.com/p/CnMwVgtLEv3/" target="_blank" rel="noopener noreferrer"><Image src="/insta2.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
+      <li><a href="https://www.instagram.com/p/CnFYqWwJyXj/" target="_blank" rel="noopener noreferrer"><Image src="/insta3.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
+      <li><a href="https://www.instagram.com/p/CnC82-ZPEXO/" target="_blank" rel="noopener noreferrer"><Image src="/insta4.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
+      <li><a href="https://www.instagram.com/p/Cm9Vi0wsn6j/" target="_blank" rel="noopener noreferrer"><Image src="/insta5.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
+      <li><a href="https://www.instagram.com/p/Cm3tn_-ucN3/" target="_blank" rel="noopener noreferrer"><Image src="/insta6.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
+      <li><a href="https://www.instagram.com/p/CmykcolAdA9/" target="_blank" rel="noopener noreferrer"><Image src="/insta7.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
+      <li><a href="https://www.instagram.com/p/Cmv_TtuMAom/" target="_blank" rel="noopener noreferrer"><Image src="/insta8.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
     </ul>
   </div>
   <div className="container">
@@ -150,6 +150,24 @@ strategy="beforeInteractive" />
     new RDStationForms('formulario-contato-atualizado-ed22e1fdee9c4ddfcecd', 'UA-48773259-1').createForm();
   }
 
+  if(jQuery('#content-instagram').length){
+    jQuery(function() {
+      jQuery.getJSON('https://api-instagram-nairuz-v2-gdy2gs33ua-uc.a.run.app/recuperarListaImagens?cliente=nairuz', function(insta) {
+        jQuery.each(insta, function(photos, src) {
+          if (photos === 8) {
+            return false;
+          }
+          console.log(src)
+          jQuery('<a href="' + src.link + '" class="post item" target="_blank">' +
+            '<div class="image" style="background-image:url(' + src.urlImagem+ ');"></div>' +
+            '<ul>' +
+            '<li><i id="twitterinsstagram" class="fa fa-instagram" onclick="socialShareWindow(&quot;https://twitter.com/intent/tweet?url=' + src.urlImagem + '&amp;screen_name=Consectetuer eget Consectetuer eget eget Consectetuer eget eget&amp;text=' + src.link + '&quot;)"></i></li>' +
+            '</ul></a>').appendTo('#content-instagram');
+        });
+      });
+    });
+  }
+
   // Carrossel geral
   $(document).ready(function(){
     $(".owl-carousel.owl-default").owlCarousel({
@@ -162,7 +180,7 @@ strategy="beforeInteractive" />
               items:1,
               nav:true
           },
-          600:{
+          800:{
               items:1,
               nav:false
           },
