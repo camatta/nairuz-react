3964300f95285846d7d421c931843be4ddd7a6a4
camatta <contatogcamatta@gmail.com>
2023-04-11 09:41:26 -0300

    att insta api

--- components/Footergeral.js
+++ components/Footergeral.js
@@ -32,8 +32,8 @@ const urlPoliticas = () => {
 const Footergeral = () => (
   <>
   <footer className="container-fluid" id="footer">
-  <div className="instagram-feed">
-    <ul className="insta d-flex justify-content-center" id="content-instagram">
+  <div className="instagram-feed" id="content-instagram">
+    {/* <ul className="insta d-flex justify-content-center">
       <li><a href="https://www.instagram.com/p/ConCrwhLL3z/" target="_blank" rel="noopener noreferrer"><Image src="/insta1.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
       <li><a href="https://www.instagram.com/p/CoetijSjQPw/" target="_blank" rel="noopener noreferrer"><Image src="/insta2.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
       <li><a href="https://www.instagram.com/p/CoVCTjSswOM/" target="_blank" rel="noopener noreferrer"><Image src="/insta3.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
@@ -42,7 +42,7 @@ const Footergeral = () => (
       <li><a href="https://www.instagram.com/p/CoFmqe8udVX/" target="_blank" rel="noopener noreferrer"><Image src="/insta6.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
       <li><a href="https://www.instagram.com/p/CoDPCGfvDOc/" target="_blank" rel="noopener noreferrer"><Image src="/insta7.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
       <li><a href="https://www.instagram.com/p/CoCYvJLpiUq/" target="_blank" rel="noopener noreferrer"><Image src="/insta8.jpg" alt="Instagram Nairuz" width={350} height={350} /></a></li>
-    </ul>
+    </ul> */}
   </div>
   <div className="container">
     <div className="row">
@@ -152,7 +152,7 @@ strategy="beforeInteractive" />
 
   if(jQuery('#content-instagram').length){
     jQuery(function() {
-      jQuery.getJSON('https://api-instagram-nairuz-v2-gdy2gs33ua-uc.a.run.app/recuperarListaImagens?cliente=nairuz', function(insta) {
+      jQuery.getJSON('https://api-instagram-nairuz-v2-gdy2gs33ua-uc.a.run.app/recuperarListaImagens?cliente=nairuzabril23', function(insta) {
         jQuery.each(insta, function(photos, src) {
           if (photos === 8) {
             return false;
@@ -162,7 +162,7 @@ strategy="beforeInteractive" />
             '<div class="image" style="background-image:url(' + src.urlImagem+ ');"></div>' +
             '<ul>' +
             '<li><i id="twitterinsstagram" class="fa fa-instagram" onclick="socialShareWindow(&quot;https://twitter.com/intent/tweet?url=' + src.urlImagem + '&amp;screen_name=Consectetuer eget Consectetuer eget eget Consectetuer eget eget&amp;text=' + src.link + '&quot;)"></i></li>' +
-            '</ul></a>').appendTo('#content-instagram');
+            '</ul></a>').appendTo('.instagram-feed');
         });
       });
     });
