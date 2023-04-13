beb71afa10be4e75bcaa90fee3faaf96a46db24a
camatta <contatogcamatta@gmail.com>
2023-04-13 14:41:27 -0300

    pontuacoes case nova med

--- pages/cases/homolog.js
+++ pages/cases/homolog.js
@@ -49,7 +49,7 @@ export default function NovaMedicamentos2() {
             <div className="row">
               <div className="col-12">
                 <div className="video-case">
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/MJBFkV03N0s" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                  <iframe width="850" height="500" src="https://www.youtube.com/embed/MJBFkV03N0s" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </div>
               </div>
             </div>
