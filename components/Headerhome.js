8f91cca642736e34bc4e97c260bd24084856e24c
kyrsten-junior <kyrsten.junior@nairuz.com.br>
2023-05-04 10:59:23 -0300

    alteração banner Maravilhas do lar

--- components/Headerhome.js
+++ components/Headerhome.js
@@ -64,7 +64,61 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   </header>
 
   <div className='owl-carousel carrossel-header'>
+
     <div className="container-fluid" id="bg-carrossel1">
+      <div className="container">
+        <div className="row" id="carrossel">
+          <div className="col-12">
+            <div className="carrossel-principal">
+              <div className="row">
+                <div className="col-12 col-md-4">
+                  <div className="left-carrossel">
+                    <p>Veja como a Maravilhas do Lar <b>aumentou sua receita em 44%</b> nos 6 primeiros meses de projeto!</p>
+                  </div>
+                </div>
+                <div className="col-3 desktoponly"></div>
+                <div className="col-12 col-md-5">
+                  <div className="right-carrossel">
+                    <div className="cards d-flex text-center">
+                      <div className="card1">
+                        <div className="img-card">
+                          <Image src="/icon-user-top.svg" alt="Usuários" width={51} height={51} />
+                        </div>
+                        <div className="title-card">
+                          <p>+ de <b>46%</b> de aumento em transações</p>
+                        </div>
+                        <div className="desc-card">
+                          <p>Média mensal de transações concluídas no e-commerce.</p>
+                        </div>
+                      </div>
+                      <div className="card2">
+                        <div className="img-card">
+                          <Image src="/icon-money-top.svg" alt="Usuários" width={51} height={51} />
+                        </div>
+                        <div className="title-card">
+                          <p>+ <b>12%</b> no tráfego</p>
+                        </div>
+                        <div className="desc-card">
+                          <p>Aumento alcançado sem alterações na verba média de Investimento em Ads.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="descricao-right">
+                      <p className="desktoponly">O Marketing Digital, mais uma vez, mostrando-se essencial para o crescimento das empresas, independente do seu porte ou história no mercado, dessa forma trazendo resultados cada vez mais expressivos e entregando o seu produto diretamente ao seu consumidor almejado. Mas como isso foi possível? Conheça nossos Cases de Sucesso!</p>
+                      <div className="button-right">
+                        <button><a onClick={urlCases}>Saiba Mais</a></button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div className="container-fluid" id="bg-carrossel2">
       <div className="container">
         <div className="row" id="carrossel">
           <div className="col-12">
@@ -118,7 +172,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </div>
       </div>
     </div>
-    <div className="container-fluid" id="bg-carrossel2">
+
+    <div className="container-fluid" id="bg-carrossel3">
       <div className="container">
         <div className="row" id="carrossel">
           <div className="col-12">
@@ -172,6 +227,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </div>
       </div>
     </div>
+
   </div>
 </>
 );
