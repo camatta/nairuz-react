6cfc0c32ffa17f566edefd8da3a756a6c9b3686c
camatta <contatogcamatta@gmail.com>
2023-06-15 14:29:35 -0300

    add capas case bonecario e covabra

--- pages/cases.js
+++ pages/cases.js
@@ -34,6 +34,15 @@ const urlCSP = () => {
   document.location.href = '/cases/casa-sao-pedro';
 }
 
+const urlCovabra = () => {
+  document.location.href = '/cases/covabra';
+}
+
+const urlBonecario = () => {
+  document.location.href = '/cases/bonecario';
+}
+
+
 export default function Cases() {
   return (
     <div className="page-cases">
@@ -82,6 +91,86 @@ export default function Cases() {
           <div className="row">
             <div className="col-12">
               <div className="cases">
+              <ul className="d-flex">
+                  <li>
+                    <div className="hide-case">
+                      <div className="tags-case">
+                        <span>Layout para blog</span>
+                      </div>
+                      <div className="descricao-case">
+                        <p>O Covabra está no mercado desde 1989, Hoje com 18 lojas na região metropolitana de Campinas. O primeiro contato com a Nairuz, que deveria ser exclusivamente técnico, evoluiu para o acompanhamento também das estratégias de aquisição de tráfego pago (mídias pagas) e SEO.</p>
+                      </div>
+                      <div className="servicos-case">
+                        <ul>
+                          <li><Image src="/cases/icon-arrow.svg" width={30} height={30} /> <p>+ 127% acessos orgânicos;</p></li>
+                          <li><Image src="/cases/icon2-faturamento.svg" width={30} height={30} /> <p>+ 92% de aumento no faturamento do e-commerce;</p></li>
+                          <li><Image src="/cases/icon3-acessos.svg" width={30} height={30} /> <p>ROAS maior que 1100% em campanhas pagas.</p></li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="button-case">
+                      <a onClick={urlCovabra}><Image src="/cases/arrow-case.svg" alt="Ver Case" width={35} height={35} /></a>
+                    </div>
+                    <div className="visible-case">
+                      <Image className="img-fluid" src="/cases/case-covabra.png" alt="Covabra" width={370} height={533} />
+                      <div className="logocase">
+                        <Image className="logo-case" src="/cases/logo-covabra.svg" alt="Covabra" width={126} height={78} />
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="hide-case">
+                      <div className="tags-case">
+                        <span>Layout Personalizado</span>
+                      </div>
+                      <div className="descricao-case">
+                        <p>A Bonecário é uma loja que envolve, principalmente, a montagem de bonecos personalizados a partir de um fluxo de etapas consecutivas que resultam em um boneco totalmente único. Os usuários conseguem fazer a montagem e acompanhar a visualização do boneco em tempo real.</p>
+                      </div>
+                      <div className="servicos-case">
+                        <ul>
+                          <li><Image src="/cases/icon3-acessos.svg" width={30} height={30} /> <p>Navegação clara e objetiva;</p></li>
+                          <li><Image src="/cases/icon1-user.svg" width={30} height={30} /> <p>Layout totalmente personalizado;</p></li>
+                          <li><Image src="/cases/icon-arrow.svg" width={30} height={30} /> <p>Mais de 1000 montagens disponíveis.</p></li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="button-case">
+                      <a onClick={urlBonecario}><Image src="/cases/arrow-case.svg" alt="Ver Case" width={35} height={35} /></a>
+                    </div>
+                    <div className="visible-case">
+                      <Image className="img-fluid" src="/cases/case-bonecario.png" alt="Bonecário" width={370} height={533} />
+                      <div className="logocase">
+                        <Image className="logo-case" src="/cases/logo-bonecario.svg" alt="Bonecário" width={126} height={78} />
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="hide-case">
+                      <div className="tags-case">
+                        <span>Layout para e-commerce</span>
+                      </div>
+                      <div className="descricao-case">
+                        <p>Atuando há mais de 60 anos no ramo de materiais de construção, reforma e acabamento, a Casa São Pedro é uma empresa tradicional. Agora, eles buscam ampliar as suas vendas em São Paulo e região através do e-commerce. Por isso, a importância de investir na experiência do usuário no seu site. Vamos descobrir mais sobre como foi a trajetória da Casa São Pedro com a Nairuz?</p>
+                      </div>
+                      <div className="servicos-case">
+                        <ul>
+                          <li><Image src="/cases/icon1-user.svg" width={30} height={30} /> <p>Criação de calculadora intuitiva;</p></li>
+                          <li><Image src="/cases/icon3-acessos.svg" width={30} height={30} /> <p>Personalização de widgets nativos;</p></li>
+                          <li><Image src="/cases/icon-arrow.svg" width={30} height={30} /> <p>Design totalmente responsivo.</p></li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="button-case">
+                      <a onClick={urlCSP}><Image src="/cases/arrow-case.svg" alt="Ver Case" width={35} height={35} /></a>
+                    </div>
+                    <div className="visible-case">
+                      <Image className="img-fluid" src="/cases/case-csp.png" alt="Infocards" width={370} height={533} />
+                      <div className="logocase">
+                        <Image className="logo-case" src="/cases/logo-csp.png" alt="Infocards" width={126} height={78} />
+                      </div>
+                    </div>
+                  </li>
+                </ul>
                 <ul className="d-flex">
                   <li>
                     <div className="hide-case">
@@ -219,87 +308,6 @@ export default function Cases() {
                     </div>
                   </li>
                   <li>
-                    <div className="hide-case">
-                      <div className="tags-case">
-                        <span>Marketing Parcial</span>
-                      </div>
-                      <div className="descricao-case">
-                        <p>A Nova Medicamentos é uma loja online, com sede em Vinhedo (SP) e uma unidade em Vila Velha (ES), que atende todo o território nacional a partir da venda e entrega de medicamentos de alto custo. A Nova Medicamentos é um case de sucesso quando falamos sobre estratégias de SEO e aumento do tráfego orgânico. Os resultados:</p>
-                      </div>
-                      <div className="servicos-case">
-                        <ul>
-                          <li><Image src="/cases/icon1-user.svg" width={30} height={30} /> <p>139,73% em tráfego orgânico;</p></li>
-                          <li><Image src="/cases/icon3-acessos.svg" width={30} height={30} /> <p>Palavras-chave nas 3 primeiras posições de busca.</p></li>
-                          <li><Image src="/cases/icon-arrow.svg" width={30} height={30} /> <p>63% em vendas orgânicas;</p></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div className="button-case">
-                      <a onClick={urlNovaMed}><Image src="/cases/arrow-case.svg" alt="Ver Case" width={35} height={35} /></a>
-                    </div>
-                    <div className="visible-case">
-                      <Image className="img-fluid" src="/cases/case-nova.png" alt="Nova Medicamentos" width={370} height={533} />
-                      <div className="logocase">
-                        <Image className="logo-case" src="/cases/logo-nova.svg" alt="Nova Medicamentos" width={100} height={50} />
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-                <ul className="d-flex">
-                  <li>
-                    <div className="hide-case">
-                      <div className="tags-case">
-                        <span>Layout para e-commerce</span>
-                      </div>
-                      <div className="descricao-case">
-                        <p>Atuando há mais de 60 anos no ramo de materiais de construção, reforma e acabamento, a Casa São Pedro é uma empresa tradicional. Agora, eles buscam ampliar as suas vendas em São Paulo e região através do e-commerce. Por isso, a importância de investir na experiência do usuário no seu site. Vamos descobrir mais sobre como foi a trajetória da Casa São Pedro com a Nairuz?</p>
-                      </div>
-                      <div className="servicos-case">
-                        <ul>
-                          <li><Image src="/cases/icon1-user.svg" width={30} height={30} /> <p>Criação de calculadora intuitiva;</p></li>
-                          <li><Image src="/cases/icon3-acessos.svg" width={30} height={30} /> <p>Personalização de widgets nativos;</p></li>
-                          <li><Image src="/cases/icon-arrow.svg" width={30} height={30} /> <p>Design totalmente responsivo.</p></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div className="button-case">
-                      <a onClick={urlCSP}><Image src="/cases/arrow-case.svg" alt="Ver Case" width={35} height={35} /></a>
-                    </div>
-                    <div className="visible-case">
-                      <Image className="img-fluid" src="/cases/case-csp.png" alt="Infocards" width={370} height={533} />
-                      <div className="logocase">
-                        <Image className="logo-case" src="/cases/logo-csp.png" alt="Infocards" width={126} height={78} />
-                      </div>
-                    </div>
-                  </li>
-                  <li className="op0">
-                    <div className="hide-case">
-                      <div className="tags-case">
-                        <span>Layout para e-commerce</span>
-                        <span>Marketing Full-Service</span>
-                      </div>
-                      <div className="descricao-case">
-                        <p>A Lojas RPM encontra-se há 5 anos localizada na cidade de Jaci, São Paulo, e conta com diversos produtos de qualidade e custo acessível para residências e comércios. Nossa equipe de consultoria em marketing digital encontrou oportunidades de crescimento e, depois de uma cuidadosa análise, definiram-se as estratégias do plano de ação. Confira!</p>
-                      </div>
-                      <div className="servicos-case">
-                        <ul>
-                          <li><Image src="/cases/icon2-faturamento.svg" width={30} height={30} /> <p>+407,12% em faturamento;</p></li>
-                          <li><Image src="/cases/icon3-acessos.svg" width={30} height={30} /> <p>+357,10% em taxa de conversão no site;</p></li>
-                          <li><Image src="/cases/icon-arrow.svg" width={30} height={30} /> <p>+241,03% nas transações em mídias pagas.</p></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div className="button-case">
-                      <a onClick={urlRPM}><Image src="/cases/arrow-case.svg" alt="Ver Case" width={35} height={35} /></a>
-                    </div>
-                    <div className="visible-case">
-                      <Image className="img-fluid" src="/cases/case-rpm.png" alt="Lojas RPM" width={370} height={533} />
-                      <div className="logocase">
-                        <Image className="logo-case" src="/cases/logo-rpm.png" alt="Lojas RPM" width={168} height={23} />
-                      </div>
-                    </div>
-                  </li>
-                  <li className="op0">
                     <div className="hide-case">
                       <div className="tags-case">
                         <span>Marketing Parcial</span>
